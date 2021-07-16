c0e450ae123cbf161e81f49bf91c1ec466935a04
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-16 09:04:38 +0100

    adicionando metodo sobre no homeController

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -12,6 +12,17 @@ class HomeController {
     return view.render('home', data)
   }
 
+  async sobre({ view }){
+      const data = {
+        title: "Sobre",
+        menu: "sobre",
+        submenu: null,
+        type: "sobre"
+      }
+
+      return view.render('sobre', data)
+  }
+
 }
 
 module.exports = HomeController
