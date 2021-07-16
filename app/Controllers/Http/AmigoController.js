071f99ee19a9a9534f0c214ec525288206c25123
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-16 11:44:07 +0100

    adicionando metodo para amigos

--- app/Controllers/Http/AmigoController.js
+++ app/Controllers/Http/AmigoController.js
@@ -1,6 +1,16 @@
 'use strict'
 
 class AmigoController {
+  async index({ view }){
+    const data = {
+      title: "Amigos",
+      menu: "Amigos",
+      submenu: null,
+      type: "amigos"
+    }
+
+    return view.render('amigos', data)
+  }
 }
 
 module.exports = AmigoController
