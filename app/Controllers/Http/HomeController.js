e8143eef5fbea94028b84c8c62ee7cff66d5a02d
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-16 08:41:45 +0100

    terminado metodo para pagina inicial

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -1,6 +1,17 @@
 'use strict'
 
 class HomeController {
+
+  async index({ view }){
+    const data = {
+      title: "Blog Internacional",
+      menu: "Home",
+      submenu: null,
+      type: "home",
+    }
+    return view.render('home', data)
+  }
+
 }
 
 module.exports = HomeController
